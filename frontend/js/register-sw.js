3847cce0578b467251f88ca29eb9ffc717d3922f
Radhika Garg <radhikagarg104@gmail.com>
2025-11-03 18:15:00 +0530

    Update register-sw.js

--- frontend/js/register-sw.js
+++ frontend/js/register-sw.js
@@ -1,1 +1,8 @@
-
+$regpath = "C:\Users\MCL\3D Objects\mgnrega_portal\frontend\js\register-sw.js"
+@"
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('js/sw.js')
+    .then(() => console.log('SW registered'))
+    .catch(console.warn);
+}
+"@ | Out-File -FilePath $regpath -Encoding UTF8
